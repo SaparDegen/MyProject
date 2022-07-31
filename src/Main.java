c02854a96dae760441a9624236c07f58a99670dc
SaparDegen <saparbekdegenbaev@gmail.com>
2022-07-31 11:06:38 +0600

    2commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
+import java.util.Arrays;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Random rnd = new Random();
+        char[] arrInt = new char[10];
+        for (int i = 0; i < arrInt.length; i++) {
+            arrInt[i] = (char) rnd.nextInt(97,115);
+        }
+        System.out.println(Arrays.toString(arrInt));
     }
 }
