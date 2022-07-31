05bd6fc609bfae500be0eac7cc87531ee5f84367
SaparDegen <saparbekdegenbaev@gmail.com>
2022-07-31 11:27:30 +0600

    7commit

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         Random rnd = new Random();
-        char[] arrInt = new char[15];
+        char[] arrInt = new char[20];
         for (int i = 0; i < arrInt.length; i++) {
             arrInt[i] = (char) rnd.nextInt(97,115);
         }
